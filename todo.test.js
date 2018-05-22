2e47ee6cb9e1dd0670daa12c37dabca57b9b6b56
Nam Tran <trnam2811@gmail.com>
2018-05-22 14:09:02 -0700

    adding buttons

--- todo.test.js
+++ todo.test.js
@@ -2,6 +2,7 @@ const todo = require('./todo.js');
 
 beforeAll(() => {
     todo.addUser("lolK@kek.com", 1, "Emmett", "answer", "mom");
+    todo.loadFile()["lolK@kek.com"].loggedin = "yes";
 });
 
 test('tests if a user is added to file', () => {
@@ -18,4 +19,33 @@ test('tests if correct artist ID is returned when requesting an artist', () => {
     return todo.getArtistID('Lil Uzi Vert', 'aFVE4X3HUdTMjVLm').then(data => {
         expect(data.id).toBe(8081893);
     });
+});
+
+describe('check if the usernames already existed in the database', () => {
+	test('username already in database -> should return 0', () => {
+		expect(todo.duplicateUsers("lolK@kek.com")).toEqual(0)
+	});
+
+	test('username not in database -> should return 1', () => {
+		expect(todo.duplicateUsers("something@something.com")).toEqual(1)
+	});
+});
+
+describe('check if the user has successfully logged in with correct username and password', () => {
+	test('user logs in with correct username and password -> should return 1', () => {
+		expect(todo.loginCheck("lolK@kek.com", 1)).toEqual(1)
+	});
+
+	test('user logs in with incorrect password -> should return 0', () => {
+		expect(todo.loginCheck("lolK@kek.com", 2)).toEqual(0)
+	})
+
+	test('user logs in with incorrect email -> should return 0', () => {
+		expect(todo.loginCheck("lolk@kek.com", 1)).toEqual(0)
+	})
+
+	test('user logs in with incorrect email and password -> should return 0', () => {
+		expect(todo.loginCheck("lolk@kek.com", 2)).toEqual(0)
+	})
+
 });
